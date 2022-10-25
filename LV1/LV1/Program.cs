beeba52adb4f452d7d6705d44de7ebd9d2a425cb
Fran Strasser <fran.strasser.1997@gmail.com>
2022-10-25 17:39:02 +0200

    drugi commit

---
+++ LV1/LV1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LV1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pozdrav iz aplikacije od Ane Marek");
+        }
+    }
+}
+
